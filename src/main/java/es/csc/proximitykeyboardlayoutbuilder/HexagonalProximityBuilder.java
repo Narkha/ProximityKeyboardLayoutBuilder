a6255dfa982c6cee5704ec66f37a7b1dfedbbef1
Narkha <granefb@gmail.com>
2015-03-04 21:18:26 +0100

    Created a brute force algorithm that arrange the keys used to write a text into an hexagonal grid and minimized the distance that a finger would expend to write it.
    Tested for 7 keys

---
+++ src/main/java/es/csc/proximitykeyboardlayoutbuilder/HexagonalProximityBuilder.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2015
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+package es.csc.proximitykeyboardlayoutbuilder;
+
+import java.util.List;
+
+public class HexagonalProximityBuilder {
+	static HexagonalWeightedGrid build(KeyFrecuencyGraph weights) {				
+		HexagonalWeightedGrid grid = new HexagonalWeightedGrid(0, weights);
+		
+		List<Key> keysByWeight = weights.keysSortedByFrecuency();
+		
+		placeMostUsedKey(keysByWeight, grid);		
+		
+		return plateOtherKeys(keysByWeight, grid);
+	}
+
+	private static void placeMostUsedKey(List<Key> keysByWeight,
+										HexagonalWeightedGrid grid) {
+		grid.nodesInRadius(0).get(0).setContent(keysByWeight.get(0));
+	}
+	
+
+	private static HexagonalWeightedGrid plateOtherKeys(List<Key> keysByWeight,
+														HexagonalWeightedGrid grid) {
+		int analyzed = 1;				
+		while( analyzed < keysByWeight.size() ) {						
+			grid.expand();
+			
+			List<Key> keysToAnalyze = keysToPlace(keysByWeight, grid, analyzed);			
+			
+			grid = minimizeWeight(grid, keysToAnalyze);
+			
+			analyzed += keysToAnalyze.size();
+		}
+		return grid;
+	}
+
+	private static List<Key> keysToPlace(List<Key> keys, 
+										HexagonalWeightedGrid grid, 
+										int analyzed) {
+		
+		int remainingKeys = keys.size() - analyzed;
+		int keysInRadius = grid.nodesInRadius( grid.radius() ).size();
+		int keysToAnalyze = Math.min(keysInRadius, remainingKeys);	
+		
+		return keys.subList(analyzed, analyzed + keysToAnalyze);
+	}
+
+	
+
+	private static HexagonalWeightedGrid minimizeWeight(HexagonalWeightedGrid grid, 
+														List<Key> keys) {
+		List<Node> nodes = grid.nodesInRadius( grid.radius() );
+		
+		nodes.get(0).setContent( keys.get(0) );
+		
+		return minimizeWeight(grid, keys, 1);
+	}
+
+	private static HexagonalWeightedGrid minimizeWeight(HexagonalWeightedGrid grid,
+														List<Key> keys,
+														int nextKey) {		
+		if (nextKey == keys.size()) {
+			return minWeightInRotation((HexagonalWeightedGrid) grid.clone());
+		}
+		else {
+			return placeNextKey(grid, keys, nextKey);
+		}
+		
+	}
+
+	private static HexagonalWeightedGrid minWeightInRotation(HexagonalWeightedGrid grid) {
+		HexagonalWeightedGrid winer = (HexagonalWeightedGrid) grid.clone();
+		
+		List<Node> nodes = grid.nodesInRadius( grid.radius() );
+		
+		for (int i = 1, n = nodes.size(); i < n; ++i) {			
+			ratateContent(nodes);		
+			
+			if (grid.totalDistance() < winer.totalDistance()) {
+				winer = (HexagonalWeightedGrid) grid.clone();
+			}
+		}
+		
+		return winer;
+	}
+
+	private static void ratateContent(List<Node> nodes) {
+		for(int j = 1, n = nodes.size(); j < n; ++j) {
+			Key content1 = nodes.get(j-1).getContent(),
+					content2 = nodes.get(j).getContent();	
+			
+			nodes.get(j - 1).setContent(content2);
+			nodes.get(j).setContent(content1);
+		}
+	}
+	
+	private static HexagonalWeightedGrid placeNextKey(
+			HexagonalWeightedGrid grid, List<Key> keys, int nextKey) {		
+		HexagonalWeightedGrid winer = null;
+		
+		List<Node> nodes = grid.nodesInRadius( grid.radius() );
+		for(Node node : nodes) {
+			if (node.isEmpty()) {
+				node.setContent( keys.get(nextKey) );
+				
+				HexagonalWeightedGrid candidate = minimizeWeight(grid, keys, nextKey + 1);
+
+				if (winer == null || candidate.totalDistance() < winer.totalDistance()) {
+					winer = candidate;
+				}
+				
+				node.resetContent();
+			}
+		}
+		
+		return winer;
+	}
+}
---
+++ src/test/java/es/csc/proximitykeyboardlayoutbuilder/HexagonalProximityBuilderTest.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2015
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+package es.csc.proximitykeyboardlayoutbuilder;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+public class HexagonalProximityBuilderTest {
+
+	@Test
+	public void ThreeKeys() throws IOException {
+		String keysFile = "data/test/HexagonalProximityBuilderTest/threeKeys.config",
+				dataFile = "data/test/HexagonalProximityBuilderTest/threeKeys.in";
+		KeyFrecuencyGraph weights = new KeyFrecuencyGraph(keysFile, dataFile);
+		
+		HexagonalWeightedGrid grid = HexagonalProximityBuilder.build(weights);
+		
+		double expectedDistance = 2 * HexagonalWeightedGrid.INNER_RADIUS * 17; 
+		assertEquals(expectedDistance, grid.totalDistance(), 0.000001);
+		
+		assertEquals("some keys are repeated", weights.size(), countFullNodes(grid));
+		
+		for(Key key : weights.keys()) {
+			assertTrue( contains(grid, key) );
+		}
+	}
+	
+	@Test
+	public void sevenKeysSimple() throws IOException {
+		String keysFile = "data/test/HexagonalProximityBuilderTest/sevenKeys.config",
+				dataFile = "data/test/HexagonalProximityBuilderTest/sevenKeysSimple.in";
+		KeyFrecuencyGraph weights = new KeyFrecuencyGraph(keysFile, dataFile);
+		
+		HexagonalWeightedGrid grid = HexagonalProximityBuilder.build(weights);
+		
+		HexagonalWeightedGrid expected = sevenKeysSimpleExpected(weights);
+		
+		assertEquals(expected.totalDistance(), grid.totalDistance(), 0.000001);		
+		
+		for(Key key : weights.keys()) {
+			assertTrue( contains(grid, key) );
+		}
+	}
+
+	private HexagonalWeightedGrid sevenKeysSimpleExpected(KeyFrecuencyGraph weights) {
+		
+		HexagonalWeightedGrid expected = new HexagonalWeightedGrid(1, weights);
+		
+		List<Node> expectedNodes = expected.nodes();		
+		expectedNodes.get(0).setContent( weights.keys().get(0) );
+		expectedNodes.get(1).setContent( weights.keys().get(1) );
+		expectedNodes.get(2).setContent( weights.keys().get(5) );
+		expectedNodes.get(3).setContent( weights.keys().get(6) );
+		expectedNodes.get(4).setContent( weights.keys().get(2) );
+		expectedNodes.get(5).setContent( weights.keys().get(3) );
+		expectedNodes.get(6).setContent( weights.keys().get(4) );
+		
+		return expected;
+	}
+	
+	@Test
+	public void sevenKeysComplex() throws IOException {
+		String keysFile = "data/test/HexagonalProximityBuilderTest/sevenKeys.config",
+				dataFile = "data/test/HexagonalProximityBuilderTest/sevenKeysComplex.in";
+		KeyFrecuencyGraph weights = new KeyFrecuencyGraph(keysFile, dataFile);
+		
+		HexagonalWeightedGrid grid = HexagonalProximityBuilder.build(weights);
+		
+		HexagonalWeightedGrid expected = sevenKeysComplexExptected(weights);
+		
+		assertEquals(expected.totalDistance(), grid.totalDistance(), 0.000001);
+		
+		for(Key key : weights.keys()) {
+			assertTrue( contains(grid, key) );
+		}
+	}
+
+	private HexagonalWeightedGrid sevenKeysComplexExptected(KeyFrecuencyGraph weights) {
+		
+		HexagonalWeightedGrid expected = new HexagonalWeightedGrid(1, weights);
+		
+		List<Node> expectedNodes = expected.nodes();		
+		expectedNodes.get(0).setContent( weights.keys().get(0) );
+		expectedNodes.get(1).setContent( weights.keys().get(1) );
+		expectedNodes.get(2).setContent( weights.keys().get(3) );
+		expectedNodes.get(3).setContent( weights.keys().get(5) );
+		expectedNodes.get(4).setContent( weights.keys().get(4) );
+		expectedNodes.get(5).setContent( weights.keys().get(6) );
+		expectedNodes.get(6).setContent( weights.keys().get(2) );
+		
+		return expected;
+	}
+
+	private Object countFullNodes(HexagonalWeightedGrid grid) {
+		int fullNodes = 0;
+		
+		for(Node node : grid.nodes()) {
+			if (!node.isEmpty()) {
+				++fullNodes;
+			}
+		}
+		
+		return fullNodes;
+	}
+
+	private boolean contains(HexagonalWeightedGrid grid, Key key) {
+		for(Node node : grid.nodes()) {
+			if (node.getContent() == key) {
+				return true;				
+			}
+		}
+		
+		return false;
+	}
+
+}
